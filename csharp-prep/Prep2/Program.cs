1cd3f6cb4b09a76d32a8496d7572b90141ca2011
unknown <carson.ryoung@gmail.com>
2026-06-13 00:33:50 -0600

    Wrote parts 1 and 2 of the core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is you grade percentage? ");
+        string percentGrade = Console.ReadLine();
+        int numberGrade = int.Parse(percentGrade);
+
+        if (numberGrade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (numberGrade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (numberGrade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (numberGrade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("Good job, you passed.");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, you failed. Better luck next time.");
+        }
     }
 }
